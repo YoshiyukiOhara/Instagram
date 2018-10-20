127976ee963e9a8a72c370ebd16e9f3276f8c823
yoshiyuki.ohara <parallel.0917@gmail.com>
2018-10-20 13:56:22 +0900

    Instagram作成

---
+++ Instagram/Const.swift
@@ -0,0 +1,13 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by Yoshi on 2018/10/18.
+//  Copyright © 2018年 yoshiyuki.oohara. All rights reserved.
+//
+
+import Foundation
+
+struct Const {
+    static let PostPath = "posts"
+}
